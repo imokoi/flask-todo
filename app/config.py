73cbd9514db7580392fa76a55c054780af568d21
tuffytian <tiantengfei@outlook.com>
2020-04-17 13:11:33 +0900

    add config file

--- app/config.py
+++ app/config.py
@@ -14,3 +14,29 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 # os.path.dirname will remove the last segment of a path, so will be
 # /Users/xxx/Documents/Codes/PythonProjects/flask-todo
 BASE_DIR = os.path.dirname(PROJECT_DIR)
+
+# config the prefix of database's path
+WIN = sys.platform.startswith("win")
+if WIN:
+    prefix = "sqlite:///"
+else:
+    prefix = "sqlite:////"
+
+
+class Config(object):
+    # Set up the secret_key of the app
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'TuffyTian'
+    # config of flask-sqlalchemy
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_RECORD_QUERIES = True
+
+
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = prefix + os.path.join(BASE_DIR, "todo-dev.sqlite")
+
+
+
+config = {
+    "development": DevelopmentConfig
+}
+
