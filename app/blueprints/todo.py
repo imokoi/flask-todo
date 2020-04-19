57a4d7cf589b651aa4ed32753195b32535c7f85d
TuffyTian <tiantengfei@outlook.com>
2020-04-19 16:40:04 +0900

    add todo list

--- app/blueprints/todo.py
+++ app/blueprints/todo.py
@@ -5,9 +5,10 @@
  * @Last Modified time: 2020/4/19 11:53 AM
 """
 
-from flask import Blueprint, render_template, url_for,redirect
+from flask import Blueprint, render_template, url_for,redirect, request
 from flask_login import login_required, current_user
 from ..models import Todo, User, TodoList
+from ..extensions import db
 
 todo_bp = Blueprint('todo', __name__)
 
@@ -18,7 +19,17 @@ def index():
         return redirect(url_for('login.login'))
     else:
         current_user_id = current_user.id
-        todo_lists: [TodoList] = TodoList.query.filter_by(id=current_user_id)
-        print(len(todo_lists.first().todos))
+        todo_lists: [TodoList] = TodoList.query.filter_by(user_id=current_user_id)
         return render_template("index.html", todoLists=todo_lists)
 
+
+@todo_bp.route("/add_list/", methods=['POST'])
+def add_todo_list() -> None:
+    list_title = request.form['title']
+    new_todo_list = TodoList(
+        title=list_title,
+        user_id=current_user.id
+    )
+    db.session.add(new_todo_list)
+    db.session.commit()
+    return redirect(url_for("todo.index"))
