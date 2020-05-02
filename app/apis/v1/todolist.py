03aa14f671e9036712ddd30985b8364e1857ef4e
tuffytian <tiantengfei@outlook.com>
2020-05-02 15:31:48 +0900

    udpate

--- app/apis/v1/todolist.py
+++ app/apis/v1/todolist.py
@@ -11,13 +11,16 @@ from ...common import success_result, failure_result
 from ...auth.auth import Auth
 from ...models import TodoList
 from ...error import PermissionError
+import time
 
 
 @api.route("/user/todo_list", methods=["GET"])
 @Auth.verify_user_permission
 def get_todo_lists():
     """ Get the all list of todoList """
-    todo_list = TodoList.query.filter_by(user_id=g.current_user.id)
+    todo_list: TodoList = TodoList.query.filter_by(user_id=g.current_user.id)
+    todo_list.create_time = time.strftime("%Y-%m-%d %H:%M:%S",
+                                          time.gmtime(todo_list.create_time))
     return jsonify(success_result(data=[list.to_json() for list in todo_list]))
 
 
@@ -35,7 +38,8 @@ def add_todo_list():
 def delete_todo_list():
     list_id = request.args.get("listId")
     try:
-        TodoList.delete_todo_list(list_id=list_id, current_user_id=g.current_user.id)
+        TodoList.delete_todo_list(list_id=list_id,
+                                  current_user_id=g.current_user.id)
     except PermissionError:
         return jsonify(failure_result(code=401, message="No Permission"))
     return jsonify(success_result())
@@ -47,7 +51,9 @@ def update_todo_list():
     list_id = request.args.get("listId")
     new_title = request.args.get("title")
     try:
-        TodoList.update_todo_list(list_id=list_id, new_title=new_title, current_user_id=g.current_user.id)
+        TodoList.update_todo_list(list_id=list_id,
+                                  new_title=new_title,
+                                  current_user_id=g.current_user.id)
     except PermissionError:
         return jsonify(failure_result(code=401, message="No Permission"))
     return jsonify(success_result())
